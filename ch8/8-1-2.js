916f65e62845eb870747df41fe2ec7d52cd44b73
gsham <gsham@12cm.co.kr>
2022-08-17 15:58:11 +0900

    8장

--- ch8/8-1-2.js
+++ ch8/8-1-2.js
@@ -1,33 +1,40 @@
 export class Account {
-  constructor(accountType, daysOverdrawn) {
-    this.type = accountType;
-    this._daysOverdrawn = daysOverdrawn;
-  }
+    constructor(accountType, daysOverdrawn) {
+        this.type = accountType;
+        this._daysOverdrawn = daysOverdrawn;
+    }
 
-  get bankCharge() {
-    let result = 4.5;
-    if (this._daysOverdrawn > 0) result += this.overdraftCharge;
-    return result;
-  }
+    get bankCharge() {
+        let result = 4.5;
+        if (this._daysOverdrawn > 0) result += this.overdraftCharge;
+        return result;
+    }
 
-  get overdraftCharge() {
-    if (this.type.isPremium) {
-      const baseCharge = 10;
-      if (this._daysOverdrawn <= 7) return baseCharge;
-      else return baseCharge + (this._daysOverdrawn - 7) * 0.85;
-    } else return this._daysOverdrawn * 1.75;
-  }
-
-  get daysOverdrawn() {
-    return this._daysOverdrawn;
-  }
+    get daysOverdrawn() {
+        return this._daysOverdrawn;
+    }
 }
 
+
+// 강의자는 타입이라는 정의를 했으므로 account 에 overdraft 에 넣는게 낮다고 본다
 export class AccountType {
-  constructor(type) {
-    this._type = type;
-  }
-  get isPremium() {
-    return this._type === 'Premium';
-  }
+    constructor(type) {
+        this._type = type;
+    }
+
+    get isPremium() {
+        return this._type === 'Premium';
+    }
+
+    // if else 문을 위에서 리턴을 하게끔 수정하고 if else 문을 삼항 연산자로 처리해서 가독성을 높여줌
+    overdraftCharge(daysOverdrawn) {
+        if(!this.isPremium) {
+            return daysOverdrawn * 1.75
+        }
+
+        const baseCharge = 10;
+
+        return daysOverdrawn <= 7 ? baseCharge : baseCharge + (daysOverdrawn - 7) * 0.85
+    }
+
 }
